ea0e57a6850dedd114ac1e5ee19185775684f69c
Moses <imunal12345@gmail.com>
2021-01-27 10:56:21 +0100

    .

--- src/Views/Player/Information/playerInformation.jsx
+++ src/Views/Player/Information/playerInformation.jsx
@@ -7,7 +7,7 @@ function PlayerInformation() {
       <h5 className="fw-900">Podstawowe informację o twoim koncie:</h5>
       <hr />
       <div className="row">
-        <div className="col-md-3 align-self-center">
+        <div className="col-md-2 align-self-center">
           <img
             className="panel__body__image img-fluid"
             src={`https://cdn.insidemta.pl/skins/${playerData.skin}.png`}
@@ -15,29 +15,32 @@ function PlayerInformation() {
             loading="lazy"
           />
         </div>
-        <div className="col-md-4 align-self-center">
-          <p className="mb-1">UID: {playerData.UID}</p>
-          <p className="mb-1">Nazwa: {playerData.username}</p>
-          <p className="mb-1">Adres e-mail: {playerData.email}</p>
-          <p className="mb-1">Utworzono dnia: {playerData.created}</p>
-          <p className="mb-1">Ostatnio w grze: {playerData.lastOnline}</p>
+        <div className="col-md-5 align-self-center">
+          <p className="mb-1">UID: <b>{playerData.UID}</b></p>
+          <p className="mb-1">Nazwa: <b>{playerData.username}</b></p>
+          <p className="mb-1">Adres e-mail: <b>{playerData.email}</b></p>
+          <p className="mb-1">Utworzono dnia: <b>{playerData.created}</b></p>
+          <p className="mb-1">Ostatnio w grze: <b>{playerData.lastOnline}</b></p>
         </div>
-        <div className="col-md-4 align-self-center">
+        <div className="col-md-5 align-self-center">
           <div className="mb-1 d-flex align-items-center">
             Życie:
             <div className="progress w-100 ms-1">
               <div
-                className="progress-bar bg-danger w-50"
+                className="progress-bar bg-danger"
                 role="progressbar"
-                aria-valuenow={playerData.health}
+                style={{
+                  width: parseInt(playerData.health),
+                }}
+                aria-valuenow={parseInt(playerData.health)}
                 aria-valuemin="0"
                 aria-valuemax="100"
               ></div>
             </div>
           </div>
-          <p className="mb-1">Gotówka: ${playerData.money}</p>
-          <p className="mb-1">Saldo konta bankowego: ${playerData.bankmoney}</p>
-          <p className="mb-1">Ostatnio w grze: {playerData.lastOnline}</p>
+          <p className="mb-1">Gotówka: <b>${playerData.money}</b></p>
+          <p className="mb-1">Saldo konta bankowego: <b>${playerData.bankmoney}</b></p>
+          <p className="mb-1">Ostatnio w grze: <b>{playerData.lastOnline}</b></p>
         </div>
       </div>
     </>
