fb7ce64896c004f241736b9f73416a438bf7fdf6
Moses <imunal12345@gmail.com>
2021-04-24 22:38:49 +0200

    .

--- src/Configs/axios.js
+++ src/Configs/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: `https://api.insidemta.pl/api/`,
-    // baseURL: `http://127.0.0.1:3333/api/`,
+    //baseURL: `http://127.0.0.1:3333/api/`,
 });
