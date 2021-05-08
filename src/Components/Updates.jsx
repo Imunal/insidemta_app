80941df3ec4c57fae5491a81260b5630ec0cbda8
Xantris <xantrisscripts@gmail.com>
2021-05-08 20:10:17 +0200

    Update#011

--- src/Components/Updates.jsx
+++ src/Components/Updates.jsx
@@ -3,14 +3,14 @@ const Updates = () => {
         <div className="slider__update mt-5 p-5">
             <span className="text-left">08.05.2021</span>
             <h1>Update #011</h1>
-            <p>Kamieniołom, minimalistyczny chat i inne usprawnienia!</p>
+            <p>Kamieniołom, minimalistyczny chat i inne usprawnienia.</p>
             <a
                 href="https://forum.insidemta.pl/topic/3163-update-011-kamieniołom-minimalistyczny-chat-i-inne-usprawnienia"
                 target="_blank"
                 rel="noreferrer"
                 className="btn btn__insidemta"
             >
-                Sprawdź co nowego
+                Sprawdź co nowego!
             </a>
         </div>
     );
