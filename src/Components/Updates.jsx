a95131da1afed145694eab2f5d89c9dfc33759a4
Xantris <xantrisscripts@gmail.com>
2021-05-08 20:07:04 +0200

    .

--- src/Components/Updates.jsx
+++ src/Components/Updates.jsx
@@ -3,7 +3,7 @@ const Updates = () => {
         <div className="slider__update mt-5 p-5">
             <span className="text-left">08.05.2021</span>
             <h1>Update #011</h1>
-            <p>Schowki, salony i inne!</p>
+            <p>Kamieniołom, minimalistyczny chat i inne usprawnienia!</p>
             <a
                 href="https://forum.insidemta.pl/topic/3163-update-011-kamieniołom-minimalistyczny-chat-i-inne-usprawnienia"
                 target="_blank"
