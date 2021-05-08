03053a05ca5dce61f59b1900dc052510bad4fdf5
Xantris <xantrisscripts@gmail.com>
2021-05-08 20:05:50 +0200

    .

--- src/Components/Updates.jsx
+++ src/Components/Updates.jsx
@@ -1,11 +1,11 @@
 const Updates = () => {
     return (
         <div className="slider__update mt-5 p-5">
-            <span className="text-left">01.04.2021</span>
-            <h1>Update #009</h1>
+            <span className="text-left">08.05.2021</span>
+            <h1>Update #011</h1>
             <p>Schowki, salony i inne!</p>
             <a
-                href="https://forum.insidemta.pl/topic/2310-update-009-schowki-salony-i-inne"
+                href="https://forum.insidemta.pl/topic/3163-update-011-kamieniołom-minimalistyczny-chat-i-inne-usprawnienia"
                 target="_blank"
                 rel="noreferrer"
                 className="btn btn__insidemta"
