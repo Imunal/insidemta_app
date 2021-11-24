b52af989a56c6719be6f38300971067a7b6bc464
Moses <imunal12345@gmail.com>
2021-11-24 21:44:23 +0100

    .

--- src/Views/InformationView.jsx
+++ src/Views/InformationView.jsx
@@ -5,70 +5,85 @@ import DiscordWebP from '../Assets/Images/Social/discord.webp';
 
 import Logo from '../Assets/Images/Logo/logo_insidemta.png';
 import LogoWebP from '../Assets/Images/Logo/logo_insidemta.webp';*/
+
+import LogoOld from '../Assets/Images/Logo/logo_insidemta_old.png';
+import Bussines from '../Assets/Images/Landing/1.png';
+import Econommy from '../Assets/Images/Landing/2.png';
+import VoiceChat from '../Assets/Images/Landing/3.png';
+import Proposition from '../Assets/Images/Landing/4.png';
+import Administration from '../Assets/Images/Landing/5.png';
+import Criminal from '../Assets/Images/Landing/6.png';
+import Ending from '../Assets/Images/Landing/7.png';
 const InformationView = () => {
     return (
         <>
             <header>
                 <div className="container">
-                    <h1 className="text-center">Welcome in Los Santos</h1>
+                    {/* <h1 className="text-center fw-bolder">Welcome in San Fierro</h1> */}
+                    <img src={LogoOld} alt="InsideMTA" className="img-fluid intro-logo" />
                 </div>
             </header>
             <div className="container">
                 <div className="intro">
                     <div className="row">
-                        <div className="col-md-6">
-                            <h3>Wracamy od Los Santos!</h3>
+                        <div className="col-md-6 align-self-center">
+                            <h3>Przedsiębiorstwa</h3>
                             <p className="mt-3">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam est
-                                odit quas ratione. Molestias quidem doloremque, harum non esse
-                                obcaecati reiciendis magni omnis dignissimos dicta accusantium
-                                repellendus in ipsam error.
+                                Każdy chciałby spełnić swój Amerykański sen... I wprowadzić nowe
+                                działalności gospodarcze do stanu San Andreas. Chcesz prowadzić i
+                                rozwijać swój warsztat samochodowy, komis samochodowy, siłownie lub
+                                klub nocny? Zatrudniać pracowników i zarządzać swoją firmą? W nowej
+                                edycji każdy będzie mógł zostać biznesmenem!
                             </p>
                         </div>
                         <div className="col-md-6">
-                            <p>Obrazek</p>
+                            <img src={Bussines} className="img-fluid" alt="Przedsiębiorstwa" />
                         </div>
                     </div>
                 </div>
                 <div className="intro">
                     <div className="row">
                         <div className="col-md-6">
-                            <p>Obrazek</p>
+                            <img src={Econommy} className="img-fluid" alt="Przebudowa gospodarki" />
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-6 align-self-center">
                             <h3>Przebudowa gospodarki</h3>
                             <p className="mt-3">
                                 Podczas ostatniej edycji pojawiło się bardzo dużo błędów i
-                                niedoróbek w naszej logice gospodarczej jak i skryptach. Mogliście
-                                zauwazyć że niektóre osoby po kilku godinach od startu posiadały już
-                                "zbugowane" pieniądze które udało im się zdobyć poprzed
-                                niedopatrzecie w kasynie. Zbyt słaby balans zarobków na pracach
-                                serwerowych również powodował duże rozbierzności. W tej edycji
-                                skupimy się głównie na przbuedowanie gospodarki, rebalansie prac
+                                Niedoróbek w naszej logice gospodarczej jak i skryptach. Mogliście
+                                zauważyć, że niektóre osoby po kilku godzinach od startu posiadały
+                                już "zbugowane" pieniądze, które udało im się zdobyć poprzez
+                                Niedopatrzenie w kasynie. Zbyt słaby balans zarobków na pracach
+                                serwerowych również powodował duże rozbieżności. W tej edycji
+                                skupimy się głównie na przebudowę gospodarki, re-balansie prac
                                 dorywczych jak i poprawieniu wszystkich znanych nam błędów
-                                pieniężdznych jakie znamy.
+                                pieniężnych, jakie znamy.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="intro">
                     <div className="row">
-                        <div className="col-md-6">
+                        <div className="col-md-6 align-self-center">
                             <h3>Restrukturyzacja administracji</h3>
                             <p className="mt-3">
                                 Nie da się ująć, że administracja poprzedniej edycji była
-                                niekompetentna, brakowało osoby która zajmowała się ekipą na
-                                poważnie. Ekipa była głównym powodem kryzysu poprzedniej edycji. W
+                                niekompetentna, brakowało osoby, która zajmowała się ekipą na
+                                Poważnie. Ekipa była głównym powodem kryzysu poprzedniej edycji. W
                                 tej edycji skupimy się na mniejszej, bardziej ogarniętej i zgranej
-                                ekipy. Przywróciliśmy na rangę Administratora kilka znanych wam
-                                pewnie osób z poprzednij edycji do której mamy zaufanie i wieje od
-                                nich profesjonalizmem. Nie wykluczamy jednak powiemu nowych osób w
+                                Ekipy. Przywróciliśmy na rangę Administratora kilka znanych wam
+                                pewnie osób z poprzedniej edycji, do której mamy zaufanie i wieje od
+                                Nich profesjonalizmem. Nie wykluczamy jednak powiewu nowych osób w
                                 naszym elitarnym gronie, dlatego polecamy wyczekiwać informacji o
                                 najbliższych rekrutacjach.
                             </p>
                         </div>
                         <div className="col-md-6">
-                            <p>Obrazek</p>
+                            <img
+                                src={Administration}
+                                className="img-fluid"
+                                alt="Restrukturyzacja administracji"
+                            />
                         </div>
                     </div>
                 </div>
@@ -76,17 +91,69 @@ const InformationView = () => {
                 <div className="intro">
                     <div className="row">
                         <div className="col-md-6">
-                            <p>Obrazek</p>
+                            <img src={VoiceChat} className="img-fluid" alt="VoiceChat" />
+                        </div>
+                        <div className="col-md-6 align-self-center">
+                            <h3>Voice Chat</h3>
+                            <p className="mt-3">
+                                Nie da się ująć, że administracja poprzedniej edycji była
+                                niekompetentna, brakowało osoby która zajmowała się ekipą na
+                                poważnie. Ekipa była głównym powodem kryzysu poprzedniej edycji. W
+                                tej edycji skupimy się na mniejszej, bardziej ogarniętej i zgranej
+                                ekipy. Przywróciliśmy na rangę Administratora kilka znanych wam
+                                pewnie osób z poprzednij edycji do której mamy zaufanie i wieje od
+                                nich profesjonalizmem. Nie wykluczamy jednak powiewu nowych osób w
+                                naszym elitarnym gronie, dlatego polecamy wyczekiwać informacji o
+                                najbliższych rekrutacjach.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="intro">
+                    <div className="row">
+                        <div className="col-md-6 align-self-center">
+                            <h3>Organizacje przestępcze</h3>
+                            <p className="mt-3">
+                                Nie da się ująć, że administracja poprzedniej edycji była
+                                niekompetentna, brakowało osoby która zajmowała się ekipą na
+                                poważnie. Ekipa była głównym powodem kryzysu poprzedniej edycji. W
+                                tej edycji skupimy się na mniejszej, bardziej ogarniętej i zgranej
+                                ekipy. Przywróciliśmy na rangę Administratora kilka znanych wam
+                                pewnie osób z poprzednij edycji do której mamy zaufanie i wieje od
+                                nich profesjonalizmem. Nie wykluczamy jednak powiewu nowych osób w
+                                naszym elitarnym gronie, dlatego polecamy wyczekiwać informacji o
+                                najbliższych rekrutacjach.
+                            </p>
                         </div>
                         <div className="col-md-6">
+                            <img
+                                src={Criminal}
+                                className="img-fluid"
+                                alt="Organizacje przestępcze"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="intro">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <img
+                                src={Proposition}
+                                className="img-fluid"
+                                alt="Realizacja waszych pomysłów"
+                            />
+                        </div>
+                        <div className="col-md-6 align-self-center">
                             <h3>
                                 Realizacja waszych
                                 <br />
                                 pomysłów
                             </h3>
                             <p className="mt-3">
-                                W poprzednij edycji dostaliśmy od was mase fajnych propozycji na
-                                nowe systemy / rozwiązania / modyfikacje które moglibyśmy
+                                W poprzedniej edycji dostaliśmy od was masę fajnych propozycji
+                                nanowe systemy / rozwiązania / modyfikacje które moglibyśmy
                                 wprowadzić. Zapisaliśmy je i stale przeglądamy analizując je
                                 dokładnie w naszym gronie administracyjnym wybierające te które
                                 naszym zdanie są najciekawsze i które zasługują na realizacje, a na
@@ -97,13 +164,34 @@ const InformationView = () => {
                 </div>
 
                 <div className="intro">
-                    <h3 className="text-center">Przemyślenia poprzedniej edycji...</h3>
+                    <div className="row">
+                        <div className="col-md-6 align-self-center">
+                            <h3>I... To wszystko?</h3>
+                            <p className="mt-3">
+                                Oczywiście, że nie! Pomimo głównych poprawek będzie również sporo
+                                mały usprawnień w kodzie, który przejdzie całkowity re-factoring,
+                                jak i modyfikacji, które uprzyjemnią wam rozgrywkę których zabrakło
+                                w poprzedniej edycji, jak i kilka dużych systemów których nie
+                                chcemy, póki co zdradzać.
+                                <br />
+                                Więcej informacji będziecie otrzymywać w najbliższych ogłoszeniach
+                                tak więc wyczekujcie.
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <img src={Ending} className="img-fluid" alt="I... To wszystko?" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="intro">
+                    <h3 className="text-center">Przemyślenia po poprzedniej edycji...</h3>
                     <p className="text-center mt-3">
                         Patrząc przez pryzmat ubiegłych miesięcy, przez ten czas przerwy dokładnie
-                        przemyśleliśmy nasze błędy i przyczyny które wpłynęły na zakończenie naszej
+                        przemyśleliśmy nasze błędy i przyczyny, które wpłynęły na zakończenie naszej
                         kariery. Oprócz zmian wizualnych zostanie zmienione wiele zasad, które
-                        pomogą zmienić sposób prowadzenia serwera. Dołożymy wszelkich starań żeby
-                        poprowadzić druga edycję serwera profesjonalne. Wracamy ze zdwojoną siłą.
+                        pomogą zmienić sposób prowadzenia serwera. Dołożymy wszelkich starań, żeby
+                        poprowadzić druga edycję serwera profesjonalne. Wracamy ze zdwojoną siłą.{' '}
                         <br /> <b>Do zobaczenia niedługo!</b>
                     </p>
                 </div>
